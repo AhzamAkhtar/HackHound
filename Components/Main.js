d31541ba164e56ec8a75876d09887829ea27ec96
farhan <ahmadfaransiddiqui@gmail.com>
2023-03-04 14:08:01 +0530

    progress ..

---
+++ Components/Main.js
@@ -0,0 +1,89 @@
+import { useState } from "react"
+import Image from "next/image"
+import { CiLogin } from "react-icons/ci";
+import { BsArrowRight } from "react-icons/bs";
+const Main = () => {
+    const [initialized , setInitialized] = useState(true)
+    const [publicKey , setPublicKey] = useState(true)
+    return(
+        <>
+        <div id="top">
+        <div
+          style={{
+            zIndex: -1,
+            position: "fixed",
+            width: "100vw",
+            height: "100vh",
+          }}
+        >
+          <Image
+            src="/mainpic.jpg"
+            className="rounded-3xl"
+            alt="Mountains with snow"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <div>
+          <h1
+            style={{
+              paddingTop: "20vh",
+              fontFamily: "monospace",
+              fontSize:"3.5rem",
+              fontWeight: "bold",
+              textAlign: "center",
+            }}
+          >
+            Start a New Journey
+            <p>With buzz... </p>
+          </h1>
+          <div class="flex justify-center">
+            {publicKey ? (
+              <>
+                {initialized ? (
+                  <>
+                    <button
+                      //onClick={() => router.push("/main")}
+                      class={`md:mr-5 bg-white text-black py-4 px-10 rounded-3xl inline-flex items-center mx-10 mt-10 `}
+                    >
+                      <span>DIVE IN</span>
+                      <BsArrowRight className="ml-1 w-5 text-3xl" />
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <button
+                      //onClick={() => turnLoginTrue()}
+                      class={`md:ml-5 bg-white text-black py-4 px-10 rounded-3xl inline-flex items-center mx-10 mt-10 `}
+                    >
+                      <span className="text-xl">Create Your Account</span>
+                      //<CiLogin className="ml-1 w-8 text-3xl" />
+                    </button>
+                  </>
+                )}
+              </>
+            ) : (
+              <>
+                <WalletMultiButtonDynamic
+                  style={{
+                    marginRight: "10px",
+                    borderRadius: "50vw",
+                  }}
+                />
+              </>
+            )}
+          </div>
+          {/* {loginState ? (
+            <>
+              <Login />
+            </>
+          ) : (
+            <></>
+          )} */}
+        </div>
+      </div>
+        </>
+    )
+}
+
+export default Main
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { WalletConnectProvider } from '../Components/WalletConnectProvider'
 import '../styles/globals.css'
-
+import '@solana/wallet-adapter-react-ui/styles.css'
+import Navbar from '../Components/Navbar'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <WalletConnectProvider>
+    <Navbar/>
+    {/* <Hero/> */}
+    <Component {...pageProps} />
+    </WalletConnectProvider>
+    </>
+  )
 }
 
 export default MyApp
